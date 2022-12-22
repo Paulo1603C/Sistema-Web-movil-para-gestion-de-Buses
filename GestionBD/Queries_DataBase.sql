5a28b58d2f13be5e53d8e449ded92321435e546f
Diego89git <dtoapanta1240@uta.edu.ec>
2022-12-22 17:41:33 -0500

    Creacion tablas Rol y Uusario

---
+++ GestionBD/Queries_DataBase.sql
@@ -0,0 +1,36 @@
+--Gestion de usuarios
+--22/12/2022 : 17:19
+-- Ing. Toapanta Diego
+USE [MovilmitogBuses]
+--Tabla Rol
+CREATE TABLE Rol
+(
+Id int IDENTITY(1,1) primary key ,
+Nombre varchar(150) not null,
+Descripcion varchar(250),
+Estado varchar(3) Default('ACT')
+);
+--Sentencias
+INSERT INTO Rol (Nombre, Descripcion,Estado) values('','','ACT')
+UPDATE Rol Set Nombre='', Descripcion='' WHERE Id=#
+UPDATE Rol Set Estado='ELI' WHERE Id=#
+Select Nombre, Descripcion FROM Rol WHERE Estado='ACT'
+Select * FROM Rol WHERE Estado='ACT'
+
+--Tabla Uusario
+CREATE TABLE Usuario
+(
+Id int IDENTITY(1,1) primary key ,
+Nombre varchar(50),
+Apellido varchar(50),
+TipoIdentificacion char(1) NOT NULL default 'C',
+NumeroIdentificacion varchar(25),
+Telefono varchar(25),
+Correo varchar(50),
+Direccion varchar(250),
+Usuario varchar(50) NOT NULL,
+Password varchar(50) NOT NULL,
+IdRol int NOT NULL,
+Estado varchar(3) Default('ACT'),
+CONSTRAINT fk_RolUsuario FOREIGN KEY (IdRol) REFERENCES Rol (Id)
+);

21ece35534ff96dad2e7ab582e5f2ea0331e1ee8
ferguevara2000 <gatoguevara2@hotmail.com>
2023-01-01 10:18:26 -0500

    Interfaz del menu de administrador

--- APP_WEB/app_buses/src/app/app.module.ts
+++ APP_WEB/app_buses/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
+import { SideNavComponent } from './components/side-nav/side-nav.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './pages/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    SideNavComponent,
+    HeaderComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
---
+++ APP_WEB/app_buses/src/app/components/side-nav/side-nav.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-side-nav',
+  templateUrl: './side-nav.component.html',
+  styleUrls: ['./side-nav.component.css']
+})
+export class SideNavComponent {
+  
+  @Input() sideNavStatus: boolean = false
+  list = [
+      {
+        number: '1',
+        name: 'Cooperativas',
+        icon: 'bi bi-building'
+      },
+      {
+        number: '2',
+        name: 'Buses',
+        icon: 'bi bi-bus-front'
+      },
+      {
+        number: '3',
+        name: 'Frecuencias',
+        icon: 'bi bi-signpost-split-fill'
+      }
+    ]
+}

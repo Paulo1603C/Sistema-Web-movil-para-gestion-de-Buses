96aecf3ecf74f0dd191aca983f37fdffaeac5dce
Paulo Martinez <paulomartinez1999@gmail.com>
2023-01-10 03:40:52 -0500

    creado la plantilla para los item buses en la pantalla lista viajes

--- APP_MOVIL/buses_movil/lib/Pages/Page/TravelList.dart
+++ APP_MOVIL/buses_movil/lib/Pages/Page/TravelList.dart
@@ -8,6 +8,12 @@ class TravelList extends StatefulWidget {
 }
 
 class _TravelListState extends State<TravelList> {
+  List<Buses> listaBuses = [
+    Buses('ABB-569', 'James'),
+    Buses('TVO-6658', 'MENDEZ'),
+    Buses('CVV-9999', 'REYES'),
+    Buses('CFG-1245', 'FREIRE'),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +26,7 @@ class _TravelListState extends State<TravelList> {
         children: [
           Container(
             padding: EdgeInsets.symmetric(
-              vertical: 20.0, 
+              vertical: 20.0,
             ),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -34,17 +40,15 @@ class _TravelListState extends State<TravelList> {
               ],
             ),
           ),
-
           Expanded(
-            child: Container(
-              child: listaItems(),
-            ),
+            child:_listaItems(context),
           ),
         ],
       ),
     );
   }
 
+//input para buscar
   Widget buscardor() {
     return TextField(
       enableInteractiveSelection: false,
@@ -52,61 +56,56 @@ class _TravelListState extends State<TravelList> {
       decoration: InputDecoration(
         labelText: 'Buscar',
         suffixIcon: IconButton(
-          onPressed: (){
-            _metodoBuscar();
-          },
-          icon: Icon( Icons.search) ),
+            onPressed: () {
+              _metodoBuscar();
+            },
+            icon: Icon(Icons.search)),
         border: OutlineInputBorder(borderRadius: BorderRadius.circular(15.0)),
       ),
     );
   }
 
+//inputs para mas opciones de busqueda
   Widget moreOptionBuscar() {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
         Expanded(child: itemComboBox()),
-        SizedBox(width: 50.0,),
+        SizedBox(
+          width: 50.0,
+        ),
         Expanded(child: buscadorFecha())
       ],
     );
   }
 
-  Widget listaItems() {
-    return ListView(
-      children: [
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center, 
-          children: [
-            Text(
-              'Bienvenido',
-              style: TextStyle(
-                fontFamily: 'Verdana',
-                fontWeight: FontWeight.bold,
-                fontSize: 30.0,
-              ),
-            ),
-        ]
-        ),
-      ],
+//metodo cargara la lista de items buses;
+  Widget _listaItems(BuildContext context) {
+    return Container(
+      child: _listadoBuses(context),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20.0),
+      ),
     );
   }
 
-  _metodoBuscar(){
+//medoto buscar
+  _metodoBuscar() {
     setState(() {
       print("object;");
     });
   }
 
-  Widget buscadorFecha(){
+//widget calendart
+  Widget buscadorFecha() {
     return TextField(
       enableInteractiveSelection: false,
       autofocus: true,
       decoration: InputDecoration(
-        hintText:"12/12/2023",
+        hintText: "12/12/2023",
         isDense: true,
         suffix: IconButton(
-          onPressed: (){}, 
+          onPressed: () {},
           icon: Icon(Icons.calendar_month),
         ),
         border: OutlineInputBorder(borderRadius: BorderRadius.circular(15.0)),
@@ -114,24 +113,63 @@ class _TravelListState extends State<TravelList> {
     );
   }
 
-  Widget itemComboBox(){
-    List items = ["Coperativas","Buses","Otros"];
+//devuelve un combo box
+  Widget itemComboBox() {
+    List items = ["Coperativas", "Buses", "Otros"];
     String? selectedValue = "Coperativas";
 
     return DropdownButton(
       hint: Text("Seleccione un item"),
-      items: items.map( ( value ){
-        return DropdownMenuItem(
-          value: selectedValue,
-          child: Text(value))
-        ;
-      } ).toList(), 
-      onChanged:( String? newValue ){
+      items: items.map((value) {
+        return DropdownMenuItem(value: selectedValue, child: Text(value));
+      }).toList(),
+      onChanged: (String? newValue) {
         setState(() {
           selectedValue = newValue;
         });
       },
-      onTap: (){},
+      onTap: () {},
     );
+  }
+
+//plantilla para cargar el listado de los buses
+  Widget _listadoBuses(BuildContext context) {
+    List<Buses> listaBuses = [
+      Buses('ABB-569', 'James'),
+      Buses('TVO-6658', 'MENDEZ'),
+      Buses('CVV-9999', 'REYES'),
+      Buses('CFG-1245', 'FREIRE'),
+    ];
+
+    return ListView.builder(
+      itemCount: listaBuses.length,
+      itemBuilder: (context, index) {
+        return Card(
+          child: Row(
+            children: [
+              Image.asset("images/logo.jpg"),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(listaBuses[index].placa.toString()),
+                  Text(listaBuses[index].Conductor.toString()),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
+
+//clase buses
+class Buses {
+  String? placa;
+  String? Conductor;
+
+  Buses(placa, coductor) {
+    this.placa = placa;
+    this.Conductor = coductor;
   }
 }
